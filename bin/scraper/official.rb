388d9c95a857c0e365c6d61f84bd7bc5f45a1ec4
Tony Bowden <tony@tmtm.com>
2021-11-10 17:51:03 +0200

    remap PM/MOS split

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -6,15 +6,23 @@ require 'pry'
 
 class MemberList
   class Member
-    field :name do
+    POSITION_MAP = {
+      'Prime Minister, Minister of State' => ['Prime Minister', 'Minister of State']
+    }
+
+    def name
       "#{given_name} #{family_name}"
     end
 
-    field :position do
-      noko.css('.card-description p').map(&:text).map(&:tidy).reject(&:empty?)
+    def position
+      raw_position.flat_map { |posn| POSITION_MAP.fetch(posn, posn) }
     end
 
     private
+
+    def raw_position
+      noko.css('.card-description p').map(&:text).map(&:tidy).reject(&:empty?)
+    end
 
     #TODO: include these in output and check WD has them set
     def family_name
